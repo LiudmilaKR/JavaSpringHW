1c71219fed4835f8ef9c4e7a3a00da38c7c891c5
Liudmila <lidiay@yandex.ru>
2024-01-08 15:52:19 +0300

    test-hw

---
+++ seminar1HW/src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("Hello world!");
+        Person person1 = new Person("Vasily", "Ivanov", 25);
+    }
+}
